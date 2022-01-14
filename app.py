4bbede2acd591b1f90733f0e64413715bc022094
Raghavendra R <97725110+raghavendraraghu1995@users.noreply.github.com>
2022-01-14 14:59:45 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-text_variable='i learn everything with my own interest'
-count=0
-word_list=text_variable.split(" ")
-for every_word in word_list:
-    if("learn"==every_word):
-        count=count+1
-
-
-print(count)
